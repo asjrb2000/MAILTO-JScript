76e237602ec54c7a79ef7e88f5ae474f43f4c2f6
asjrb2000 <asjrb2000@yahoo.com>
2022-03-05 06:29:37 -0600

    Add files via upload

---
+++ mailto.js
@@ -0,0 +1,22 @@
+ 
+<script>
+
+const elements = document.getElementsByClassName("original-dates"); 
+const rdate = Array.from(elements); 
+const element2 = document.getElementsByTagName("h2"); 
+const course = Array.from(element2); 
+const element3 = document.getElementById("full_name"); 
+const fullname = element3.innerText; 
+document.write('<p>' + fullname + '</p>'); 
+course.forEach(console.log); 
+rdate.forEach(console.log); 
+document.write('<p>Hello John:' + course[0].innerHTML + '</p>'); 
+const subj = 'subject="' + rdate[0].innerHTML + '&amp;body=VolunteerWed" class="ce-button"'; 
+document.write('<p>' + subj + '</p>'); 
+const body = 'body=I volunteer to lead the ride on ' + course[0].innerHTML + ' --- ' + fullname; 
+document.write('<p>' + body + '</p>'); 
+const xmail = '<a href="mailto:asjrb2000 @yahoo.com?cc=asjrb2000@gmail.com&subject=Wed Ride Volunteer for' + rdate[0].innerHTML + ' &' + body + '" class="ce - button ">Volunteer</a> '; 
+console.log(xmail); 
+document.write(xmail);
+
+</script>
