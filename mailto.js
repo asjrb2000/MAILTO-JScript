911e55bfdc029d0a6d04b199c191ecc1cd055449
asjrb2000 <asjrb2000@yahoo.com>
2022-03-09 16:37:13 -0600

    Add files via upload

--- mailto.js
+++ mailto.js
@@ -1,6 +1,4 @@
- 
-<script>
-
+console.log('This is my first log message.')
 const elements = document.getElementsByClassName("original-dates"); 
 const rdate = Array.from(elements); 
 const element2 = document.getElementsByTagName("h2"); 
@@ -9,14 +7,21 @@ const element3 = document.getElementById("full_name");
 const fullname = element3.innerText; 
 document.write('<p>' + fullname + '</p>'); 
 course.forEach(console.log); 
-rdate.forEach(console.log); 
+rdate.forEach(console.log);
+ 
 document.write('<p>Hello John:' + course[0].innerHTML + '</p>'); 
-const subj = 'subject="' + rdate[0].innerHTML + '&amp;body=VolunteerWed" class="ce-button"'; 
+
+const subj = 'subject="' + rdate[0].innerHTML + '&amp;"'; 
 document.write('<p>' + subj + '</p>'); 
+
 const body = 'body=I volunteer to lead the ride on ' + course[0].innerHTML + ' --- ' + fullname; 
 document.write('<p>' + body + '</p>'); 
-const xmail = '<a href="mailto:asjrb2000 @yahoo.com?cc=asjrb2000@gmail.com&subject=Wed Ride Volunteer for' + rdate[0].innerHTML + ' &' + body + '" class="ce - button ">Volunteer</a> '; 
+
+const xmail = '<a href="mailto:asjrb2000@yahoo.com?' +
+        'cc=asjrb2000@gmail.com&' +
+        'subject=Wed Ride Volunteer for' + rdate[0].innerHTML + ' &' + 
+        body + 
+        ' " class="ce-button ">Volunteer</a> '; 
 console.log(xmail); 
 document.write(xmail);
 
-</script>
