4901742a7ee2230dbfa15ba60ebf96fca0c4f2a0
asjrb2000 <asjrb2000@yahoo.com>
2022-03-14 17:28:48 -0500

    Update mailto.js

--- mailto.js
+++ mailto.js
@@ -1,19 +1,20 @@
-const elements = document.getElementsByClassName("original-dates"); 
-const rdate = Array.from(elements);
-const element4 = document.getElementsByClassName("panel-user-email"); 
-const remail = Array.from(element4);  
-const element5 = document.getElementsByClassName("panel-user-phone"); 
-const rphone = Array.from(element5); 
-const element2 = document.getElementsByTagName("h2"); 
-const course = Array.from(element2); 
-const element3 = document.getElementById("full_name"); 
-const fullname = element3.innerText; 
+{
+let elements = document.getElementsByClassName("original-dates"); 
+let rdate = Array.from(elements);
+let element4 = document.getElementsByClassName("panel-user-email"); 
+let remail = Array.from(element4);  
+let element5 = document.getElementsByClassName("panel-user-phone"); 
+let rphone = Array.from(element5); 
+let element2 = document.getElementsByTagName("h2"); 
+let course = Array.from(element2); 
+let element3 = document.getElementById("full_name"); 
+let fullname = element3.innerText; 
  
 
-const subj = 'subject=Ride Leader Volunteer ' + rdate[0].innerText + '&amp'; 
+let subj = 'subject=Ride Leader Volunteer ' + rdate[0].innerText + '&amp'; 
 console.log(subj);
 
-const body = 'body=***Thank you for volunteering!***%0D%0A%0D%0A' +
+let body = 'body=***Thank you for volunteering!***%0D%0A%0D%0A' +
                    'I volunteer to lead the ride ' + 
                    course[0].innerHTML + 
                    ' on ' + rdate[0].innerText + '%0D%0A' +
@@ -22,7 +23,7 @@ const body = 'body=***Thank you for volunteering!***%0D%0A%0D%0A' +
                    rphone[0].innerHTML;
 console.log(body);
  
-const xmail = '<a href="mailto:WeekendRideCoord@creamcitycycleclub.com?' +
+let xmail = '<a href="mailto:WeekendRideCoord@creamcitycycleclub.com?' +
         'cc=asjrb2000@gmail.com&' +
         subj + ' &' + 
         body + 
@@ -31,3 +32,4 @@ const xmail = '<a href="mailto:WeekendRideCoord@creamcitycycleclub.com?' +
 console.log(xmail);
  
 document.write(xmail);
+}
